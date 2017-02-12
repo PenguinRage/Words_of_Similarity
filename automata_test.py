12ac004d00b89f3c8392c19209cd9aa3436bb11e
icleasby <icle6423@uni.sydney.edu.au>
2017-02-13 10:33:26 +1100

    Levenshtein Automata tests updated

--- automata_test.py
+++ automata_test.py
@@ -7,18 +7,21 @@ words = [x.strip().lower() for x in open('sowpods.txt')]
 words.sort()
 
 
-class FoodCase(unittest.TestCase):
+class Tests(unittest.TestCase):
     def test_food(self):
         m = Matcher(words)
         self.assertEqual(len((list(automata.find_all_matches('food', 1, m)))), 18)
         self.assertEqual(len((list(automata.find_all_matches('food', 2, m)))), 318)
 
-
-class HelloCase(unittest.TestCase):
     def test_hello(self):
         m = Matcher(words)
         self.assertEqual(len((list(automata.find_all_matches('hello', 1, m)))), 12)
+        self.assertEqual(len((list(automata.find_all_matches('hello', 2, m)))), 128)
 
+    def test_slice(self):
+        m = Matcher(words)
+        self.assertEqual(len((list(automata.find_all_matches('slice', 1, m)))), 15)
+        self.assertEqual(len((list(automata.find_all_matches('slice', 2, m)))), 213)
 
 class Matcher(object):
     def __init__(self, l):
